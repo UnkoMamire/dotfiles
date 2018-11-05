b33ce82734a1ffc16a235e0a0941b3349873a6cb
Shinya Kousokabe <darnisee@slackware.home>
2018-11-06 05:14:28 +0900

    スクリプトのバグを修正。拡張子の変更

--- link.sh
+++ link.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-dotdir=`dirname $0`
-cd $dotdir
+cd `dirname $0`
+dotdir=`pwd`
 
 for f in .??*; do
 	[ $f = '.git' ] && continue
 	for g in `find $f`; do
 		if [ -d $g ]; then
-			mkdir -p $g
+			mkdir -p ~/$g
 		elif [ -f $g ]; then
 			ln -sfnv $dotdir/$g ~/$g
 		fi
--- setup_scripts/prezto.zsh
+++ setup_scripts/prezto.zsh
@@ -1,10 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
-zsh << EOF
 git clone --recursive https://github.com/UnkoMamire/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-EOF
 
